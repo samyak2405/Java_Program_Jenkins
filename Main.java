ae82b6dee02406c8dfb54447475dc8ac01c67195
Samyak moon <moonsamyak54@gmail.com>
2023-12-28 21:50:35 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a java program written by samyak");
+        System.out.println("Samyak is a Java Developer");
+    }
+}
