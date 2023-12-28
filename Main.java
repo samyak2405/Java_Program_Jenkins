cb6ac9272e5b9109613b800fada786e545efd664
Samyak moon <moonsamyak54@gmail.com>
2023-12-28 21:52:01 +0530

    Second Commit

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * Main
  */
@@ -6,5 +9,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a java program written by samyak");
         System.out.println("Samyak is a Java Developer");
+        System.out.println("Date: "+LocalDate.now());
+        System.out.println("Time: "+LocalTime.now());
     }
 }
